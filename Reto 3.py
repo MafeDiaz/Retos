16875098d3eb45f0ad2c8d570143eecc27c2867f
MafeDiaz <125712070+MafeDiaz@users.noreply.github.com>
2023-03-11 07:55:05 -0500

    Update Reto 3.py

--- Reto 3.py
+++ Reto 3.py
@@ -1,14 +1,30 @@
 import random
+
 dado1 = random.randint(1,6)
 dado2 = random.randint(1,6)
 
-print("Craps apuestas a un solo tiro,este juego se lanzan dos dados, se gana si se obtine un lanzamiento con:")
-print("Un par de unos en los dados \n"
-     ,"Un par de tres en los dados \n"
-     ,"Un par de once en los dados \n"
-     ,"Un par de dos o once en los dados \n"
-     ,"Un total de sietes en los dados , en el resto de situaciones se pierde")
+print ("Craps apuestas a un solo tiro")
+print ("Usted gana si: ")
+print ("Un par de unos en los dados \n"
+       ,"Un total de 3 en los dados\n"
+       ,"Un total de 11 en los dados \n"
+       ,"Si se obtiene 2 o 12 en los dados\n"
+       ,"Un total de 7 en los dados"
+       ,"Cualquier otro resultado pierde")
 
- if dado1 ==1 and dado2 ==1 or dado1 + dado2 == 3 or dado1 + dado2 == 11 or dado1 + dado2 == 2 or dado1 + dado2 == 12 or dado1 + dado2 == 7:
-    print("Usted gano")    
-
+if dado1 ==1 and dado2 ==1:
+    print("Que suerte tiene, usted gano!!!") 
+elif dado1 + dado2 == 3:
+    print("Que suerte tiene, usted gano!!!")
+elif dado1 + dado2 == 11:
+    print("Que suerte tiene, usted gano!!!")
+elif dado1 + dado2 == 2:
+    print("Que suerte tiene, usted gano!!!")
+elif dado1 + dado2 == 12:
+    print("Que suerte tiene, usted gano!!!")
+elif dado1 + dado2 == 7:
+    print("Que suerte tiene, usted gano!!!")
+else:
+    print ("Lo sentimos usted perdio!!!")    
+print ("Dado 1: ",dado1)
+print ("Dado 2: ",dado2)
