1c14ca1a4e733f323817b6015aaa776cb0984eb5
MafeDiaz <125712070+MafeDiaz@users.noreply.github.com>
2023-03-11 07:51:07 -0500

    Update Reto2.py

--- Reto2.py
+++ Reto2.py
@@ -1,4 +1,4 @@
-peso = int(input("Ingrese el peso del bebé"))
-meses =int(input("Ingrese los meses del bebé"))
-dosis = ((peso+10)/(edad*10))*8
+peso = int(input("Ingrese el peso del bebé: "))
+meses =int(input("Ingrese los meses del bebé: "))
+dosis = ((peso+10)/(meses*10))*8
 print ("La dosis a aplicar es = ",dosis)
